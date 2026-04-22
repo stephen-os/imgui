fd275d89a323de7beeb7434a652608dfa00c52f4
Stephen Watson <imstephen.1sw@gmail.com>
2026-04-22 12:31:00 -0400

    chore(build) added premake file and added visual studio project files to git ignore.

---
+++ premake5.lua
@@ -0,0 +1,49 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    flags { "MultiProcessorCompile" }
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imgui.cpp",
+        "imgui.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+
+        -- GLFW backend (platform/input handling)
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_glfw.h"
+    }
+
+    includedirs
+    {
+        ".",
+        "../glfw/include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
